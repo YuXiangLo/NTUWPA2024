2086f5ebc77ec422a47c9361031e941dc9fae026
YuXiangLo <linus13514@gmail.com>
2025-04-05 22:21:44 +0800

    fix: wrong config which doesn't get by user.service

--- VBWEB/backend/src/app.module.ts
+++ VBWEB/backend/src/app.module.ts
@@ -2,32 +2,13 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { SupabaseModule } from './supabase/supabase.module';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
-        const secret = configService.get<string>('JWT_SECRET');
-        
-        if (!secret) {
-          throw new Error('JWT_SECRET is not defined in the environment variables');
-        }
-
-        return {
-          secret: decodeURIComponent(secret),
-          signOptions: { expiresIn: '60s' },
-        };
-      },
-    }),
-
     UserModule,
     SupabaseModule,
   ]
