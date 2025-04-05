a999169be4cdd7bd0aa02978236118de7d8107d8
YuXiangLo <linus13514@gmail.com>
2025-04-05 22:13:57 +0800

    fix: remove redundant file

--- VBWEB/backend/src/auth/auth.module.ts
+++
@@ -1,9 +0,0 @@
-// src/auth/auth.module.ts
-import { Module } from '@nestjs/common';
-import { JwtStrategy } from './jwt.strategy';
-import { UserService } from '../user/user.service';  // Inject UserService for user validation
-
-@Module({
-  providers: [JwtStrategy, UserService],  // Register JwtStrategy and UserService
-})
-export class AuthModule {}
