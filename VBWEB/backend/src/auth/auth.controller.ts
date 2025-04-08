51692c049406d7b3b7c8737c293ee762d691e8e5
YuXiangLo <linus13514@gmail.com>
2025-04-08 16:52:33 +0800

    feat: POST auth/refresh-token api done

--- VBWEB/backend/src/auth/auth.controller.ts
+++ VBWEB/backend/src/auth/auth.controller.ts
@@ -12,6 +12,7 @@ import {
   } from '@nestjs/common';
   import { AuthService } from './auth.service';
   import { CreateUserDto } from './dto/create-user.dto';
+  import { RefreshTokenDto } from './dto/refresh-token.dto';
   import { LoginDto } from './dto/login.dto';
   import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
   
@@ -58,16 +59,45 @@ import {
       const { email, password } = body;
   
       try {
-        const { accessToken } = await this.authService.loginUser(email, password);
+        const { accessToken, refreshToken } = await this.authService.loginUser(email, password);
         return {
           status: 'success',
           accessToken,
+          refreshToken
         };
       } catch (error) {
         if (error.message === 'Invalid credentials' || error.message === 'User not found') {
           throw new UnauthorizedException(error.message);
         }
         throw new InternalServerErrorException('Unexpected error during login');
+      }
+    }
+
+    @Post('refresh-token')
+    @HttpCode(200)
+    @ApiOperation({ summary: 'Refresh access token' })
+    @ApiResponse({ status: 200, description: 'Access token refreshed' })
+    @ApiResponse({ status: 401, description: 'Invalid refresh token' })
+    @ApiResponse({ status: 500, description: 'Unexpected server error' })
+    async refreshToken(@Body() body: RefreshTokenDto) {
+      const { refreshToken: oldRefreshToken } = body;
+
+      if (!oldRefreshToken) {
+        throw new BadRequestException('Refresh token is required');
+      }
+
+      try {
+        const { accessToken, refreshToken } = await this.authService.refreshAccessToken(oldRefreshToken);
+        return {
+          status: 'success',
+          accessToken,
+          refreshToken,
+        };
+      } catch (error) {
+        if (error instanceof UnauthorizedException) {
+          throw error;
+        }
+        throw new InternalServerErrorException('Unexpected error during token refresh');
       }
     }
   }
