e8f933efd91d78c834d2ee5c61843244cef6a7d7
igeryoung <asd7415953@gmail.com>
2025-05-18 00:43:20 +0800

    feat/google third part login

--- VBWEB/backend/src/auth/strategy/jwt.strategy.ts
+++ VBWEB/backend/src/auth/strategy/jwt.strategy.ts
@@ -4,7 +4,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
-import { SupabaseService } from '../supabase/supabase.service';
+import { SupabaseService } from '../../supabase/supabase.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
